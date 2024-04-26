a4bfc994cbb319a40db618b7d15e416a5c746ee8
TotiManuel <manuel.mandaio@gmail.com>
2024-04-26 14:15:49 -0300

    Agregado GUI

--- Version 1.0/BiomagnetismoPB.py
+++ Version 1.0/BiomagnetismoPB.py
@@ -1,16 +1,31 @@
 from Funciones import CargaDatos, Venta, Reembolso, AgregarProducto
-from Class.ClassProducto import Producto
+import tkinter as tk
 def cargaDatos():
     totalProductos = CargaDatos.main()
     return totalProductos
-
+def venta(datosCargados):
+    venta = Venta.main(datosCargados, "Gorra", 12.50, "U")
+def reembolso(datosCargados):
+    reembolso = Reembolso.main(datosCargados, "Gorra", 12.50, "U")
+def agregarProducto(datosCargados):
+    productoagregado = AgregarProducto.main(datosCargados, "Banda", 11.00, "M", 7)
 def main():
     datosCargados = cargaDatos()
-    x = 0
-    while x != 1:
-        venta = Venta.main(datosCargados, "Gorra", 12.50, "U")
-        reembolso = Reembolso.main(datosCargados, "Gorra", 12.50, "U")
-        productoagregado = AgregarProducto.main(datosCargados, "Banda", 11.00, "M", 7)
-        x = x + 1
+    root = tk.Tk()
+    root.title("BioMagnetismo")
+    root.geometry("400x500+0+0")
+
+    barraMenu = tk.Menu(root)
+    menuArchivo = tk.Menu(barraMenu, tearoff=0)
+
+    menuArchivo.add_command(label="Probar Venta", command=lambda: venta(datosCargados))
+    menuArchivo.add_command(label="Probar Reembolso", command=lambda: reembolso(datosCargados))
+    menuArchivo.add_command(label="Probar Agregar Producto", command=lambda: agregarProducto(datosCargados))
+    barraMenu.add_cascade(label="Archivo", menu = menuArchivo)
+    
+    root.config(menu=barraMenu)
+
+    root.mainloop()
+    
 if __name__ == "__main__":
     main()
